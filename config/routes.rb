30433b8b0204bdb16579d09e66a5889e31036cb7
David Tran <davidttemail@gmail.com>
2020-05-17 21:59:33 -0600

    Update MVC for item requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index]
+      resources :items, except: [:edit, :new] do
+        get "/merchant", to: "merchant_items#merchant"
+      end
       resources :merchants, except: [:edit, :new] do
         get "/items", to: "merchant_items#index"
       end
