50856136aa71cba278bd76f6cc2a44e4155995ed
David Tran <davidttemail@gmail.com>
2020-05-18 00:57:04 -0600

    Add MVC for finding item(s)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, except: [:edit, :new] do
-        get "/merchant", to: "merchant_items#merchant"
+
+      namespace :merchants do
+        get "/find", to: "search#show"
+        get "/find_all", to: "search#index"
       end
+
       resources :merchants, except: [:edit, :new] do
         get "/items", to: "merchant_items#index"
+      end
+
+      namespace :items do
+        get "/find", to: "search#show"
+        get "/find_all", to: "search#index"
+      end
+
+      resources :items, except: [:edit, :new] do
+        get "/merchant", to: "merchant_items#merchant"
       end
     end
   end
